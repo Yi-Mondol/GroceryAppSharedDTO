7e0738c7f0426922f6682d739466e1d0e295a6ee
Mondol Yi <yimondol22@gmail.com>
2026-09-17 21:07:24 +0900

    register and loginresponse

---
+++ Sources/GroceryAppSharedDTO/LoginResponseDTO.swift
@@ -0,0 +1,24 @@
+//
+//  File.swift
+//  GroceryAppSharedDTO
+//
+//  Created by Yi Mondol on 9/17/26.
+//
+
+import Foundation
+
+
+public struct LoginResponseDTO: Codable {
+    public let error: Bool
+    public var reason: String? = nil
+    public var token: String? = nil
+    public var userId: UUID? = nil
+    
+    public init(error: Bool, reason: String? = nil, token: String? = nil, userId: UUID? = nil) {
+        
+        self.error = error
+        self.reason = reason
+        self.token = token
+        self.userId = userId
+    }
+}
